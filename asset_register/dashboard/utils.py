198eda5cf2206ab7d703564aa17543d03d35f09e
Terry Cain <terry@terrys-home.co.uk>
2020-06-09 12:04:14 +0100

    Updated api and search

--- asset_register/dashboard/utils.py
+++ asset_register/dashboard/utils.py
@@ -8,14 +8,20 @@ def login_required(api: bool = False):
     def _f(func):
         @functools.wraps(func)
         def _w(request: HttpRequest, *args, **kwargs) -> HttpResponse:
-            if not request.user.is_authenticated:
-                # Dont redirect api requests to azure sso, it doest like it
-                if api:
-                    return JsonResponse(
-                        {"error": "User unauthenticated, please refresh the page and try again"}, status=401
-                    )
-                return HttpResponseRedirect(reverse("oidc_authentication_init"))
+            unauthed = request.user.is_authenticated
+            err_response = HttpResponseRedirect(reverse("oidc_authentication_init"))
 
+            if api:
+                err_response = JsonResponse(
+                    {"error": "User unauthenticated, please refresh the page and try again"}, status=401
+                )
+
+                # Cheapo api auth for now
+                if request.headers.get("Authorization", "").endswith("7bb4810d-6af0-484d-9595-3402c75bfdc3"):
+                    unauthed = False
+
+            if unauthed:
+                return err_response
             return func(request, *args, **kwargs)
 
         return _w
